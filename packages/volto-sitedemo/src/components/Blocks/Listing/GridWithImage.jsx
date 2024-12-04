153c74e144461f77f115053190d7822a2bfabdd0
blakolukas <lucasblacovicz@gmail.com>
2024-12-04 13:12:50 -0300

    grid com 3 colunas

---
+++ packages/volto-sitedemo/src/components/Blocks/Listing/GridWithImage.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { flattenToAppURL } from '@plone/volto/helpers';
+import { Link } from 'react-router-dom';
+
+const GridWithImage = ({ items = [] }) => {
+  console.log(items);
+  return (
+    <div className="grid-with-image">
+      <div className="grid">
+        {items.map((item) => (
+          <>
+            {item.type_title === 'Página' && (
+              <Link to={item.url}>
+                <div className="grid-item" key={item['@id']}>
+                  <div className="image">
+                    <img
+                      src={flattenToAppURL(
+                        item.image_url || '/icone-servico-servidor.png',
+                      )}
+                      alt={item.title || 'Image'}
+                    />
+                  </div>
+                  <div className="content">
+                    <h3>{item.title}</h3>
+                    <p>{item.description}</p>
+                  </div>
+                </div>
+              </Link>
+            )}
+          </>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default GridWithImage;
