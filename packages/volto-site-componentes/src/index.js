d6dc0af88d3b631eadeba7a726a3e201a0ee9475
Samoel Silva <samoel-silva@procergs.rs.gov.br>
2025-04-17 12:31:34 -0300

    #1150001 - corrige lint

--- packages/volto-site-componentes/src/index.js
+++ packages/volto-site-componentes/src/index.js
@@ -22,7 +22,6 @@ import BannerBlockView from './components/Blocks/Banner/View';
 import BannerBlockEdit from './components/Blocks/Banner/Edit';
 import bannerSVG from '@plone/volto/icons/home.svg';
 
-
 //gray and small text
 import './theme/custom.less';
 
@@ -103,16 +102,15 @@ const applyConfig = (config) => {
     },
   ];
 
-    config.blocks.blocksConfig.banner = {
-      id: 'banner',
-      title: 'Banner',
-      group: 'procergs',
-      icon: bannerSVG,
-      edit: BannerBlockEdit,
-      view: BannerBlockView,
-      sidebarTab: 1,
-    };
-
+  config.blocks.blocksConfig.banner = {
+    id: 'banner',
+    title: 'Banner',
+    group: 'procergs',
+    icon: bannerSVG,
+    edit: BannerBlockEdit,
+    view: BannerBlockView,
+    sidebarTab: 1,
+  };
 
   return config;
 };
